74dd093514f5f478ccb0b22918140353d171e30f
Dalton Okechukwu <chike911@yahoo.com>
2021-12-18 15:59:57 -0700

    formula-1: monza tyre strategy ricciardo vs verstappen

---
+++ f1-2021-italy.py
@@ -0,0 +1,127 @@
+import fastf1 as ff1
+from fastf1 import plotting
+from matplotlib import pyplot as plt
+from matplotlib.pyplot import figure
+import numpy as np
+import pandas as pd
+
+# Setup plotting
+plotting.setup_mpl()
+
+# Enable the cache
+ff1.Cache.enable_cache('./f1-cache') 
+
+# Get rid of some pandas warnings that are not relevant for us at the moment
+pd.options.mode.chained_assignment = None 
+
+# Load the session data
+race = ff1.get_session(2021, 'Monza', 'R')
+
+# Get the laps
+laps = race.load_laps(with_telemetry=True)
+
+# Get laps of the drivers (RIC and VER)
+laps_ric = laps.pick_driver('RIC')
+laps_ver = laps.pick_driver('VER')
+
+# We are only analyzing stint 1, so select that one
+laps_ric = laps_ric.loc[laps_ric['Stint'] == 1]
+laps_ver = laps_ver.loc[laps_ver['Stint'] == 1]
+
+laps_ric['RaceLapNumber'] = laps_ric['LapNumber'] - 1
+laps_ver['RaceLapNumber'] = laps_ver['LapNumber'] - 1
+
+full_distance_ver_ric = pd.DataFrame()
+summarized_distance_ver_ric = pd.DataFrame()
+
+for lap in laps_ver.iterlaps():
+    telemetry = lap[1].get_car_data().add_distance().add_driver_ahead()
+    
+    # Only run this loop when driver ahead is RIC, otherwise we compare wrong distance gaps
+    telemetry = telemetry.loc[telemetry['DriverAhead'] == "3"]
+    
+    if len(telemetry) != 0:
+        # Full distance
+        lap_telemetry = telemetry[['Distance', 'DistanceToDriverAhead']]
+        lap_telemetry.loc[:, 'Lap'] = lap[0] + 1
+
+        full_distance_ver_ric = full_distance_ver_ric.append(lap_telemetry)
+
+        # Average / median distance
+        distance_mean = np.nanmean(telemetry['DistanceToDriverAhead'])
+        distance_median = np.nanmedian(telemetry['DistanceToDriverAhead'])
+
+        summarized_distance_ver_ric = summarized_distance_ver_ric.append({
+            'Lap': lap[0] + 1,
+            'Mean': distance_mean,
+            'Median': distance_median
+        }, ignore_index = True)
+
+plt.rcParams['figure.figsize'] = [10, 6]
+
+fig, ax = plt.subplots(2)
+fig.suptitle("RIC vs VER opening stint comparison")
+
+ax[0].plot(laps_ric['RaceLapNumber'], laps_ric['LapTime'], label='RIC')
+ax[0].plot(laps_ver['RaceLapNumber'], laps_ver['LapTime'], label='VER')
+ax[0].set(ylabel='Laptime', xlabel='Lap')
+ax[0].legend(loc="upper center")
+
+ax[1].plot(summarized_distance_ver_ric['Lap'], summarized_distance_ver_ric['Mean'], label='Mean', color='red')
+ax[1].plot(summarized_distance_ver_ric['Lap'], summarized_distance_ver_ric['Median'], label='Median', color='grey')
+ax[1].set(ylabel='Distance (meters)', xlabel='Lap')
+ax[1].legend(loc="upper center")
+
+# Hide x labels and tick labels for top plots and y ticks for right plots.
+for a in ax.flat:
+    a.label_outer()
+
+plt.show()
+
+
+# Get lap data
+lap_telemetry_ric = laps_ric.loc[laps_ric['RaceLapNumber']==4].get_car_data().add_distance()
+lap_telemetry_ver = laps_ver.loc[laps_ver['RaceLapNumber']==4].get_car_data().add_distance()
+
+distance_lap3 = full_distance_ver_ric.loc[full_distance_ver_ric['Lap']==3]
+distance_lap4 = full_distance_ver_ric.loc[full_distance_ver_ric['Lap']==4]
+distance_lap5 = full_distance_ver_ric.loc[full_distance_ver_ric['Lap']==5]
+distance_lap6 = full_distance_ver_ric.loc[full_distance_ver_ric['Lap']==6]
+
+# Make plot a bit bigger
+plt.rcParams['figure.figsize'] = [15, 15]
+
+fig, ax = plt.subplots(5)
+fig.suptitle("Fastest Race Lap Telemetry Comparison")
+
+ax[0].title.set_text("Distance to RIC (m)")
+ax[0].plot(distance_lap3['Distance'], distance_lap3['DistanceToDriverAhead'], label='Lap 3', linestyle='dotted', color='grey')
+ax[0].plot(distance_lap4['Distance'], distance_lap4['DistanceToDriverAhead'], label='Lap 4')
+ax[0].plot(distance_lap5['Distance'], distance_lap5['DistanceToDriverAhead'], label='Lap 5', linestyle='dotted', color='white')
+ax[0].plot(distance_lap6['Distance'], distance_lap6['DistanceToDriverAhead'], label='Lap 6', linestyle='dashed', color='lightgrey')
+ax[0].legend(loc="lower right")
+ax[0].set(ylabel='Distance to RIC')
+
+ax[1].title.set_text("Lap 4 telemetry")
+ax[1].plot(lap_telemetry_ric['Distance'], lap_telemetry_ric['Speed'], label='RIC')
+ax[1].plot(lap_telemetry_ver['Distance'], lap_telemetry_ver['Speed'], label='VER')
+ax[1].set(ylabel='Speed')
+ax[1].legend(loc="lower right")
+
+ax[2].plot(lap_telemetry_ric['Distance'], lap_telemetry_ric['Throttle'], label='RIC')
+ax[2].plot(lap_telemetry_ver['Distance'], lap_telemetry_ver['Throttle'], label='VER')
+ax[2].set(ylabel='Throttle')
+
+ax[3].plot(lap_telemetry_ric['Distance'], lap_telemetry_ric['Brake'], label='RIC')
+ax[3].plot(lap_telemetry_ver['Distance'], lap_telemetry_ver['Brake'], label='VER')
+ax[3].set(ylabel='Brakes')
+
+ax[4].plot(lap_telemetry_ric['Distance'], lap_telemetry_ric['DRS'], label='RIC')
+ax[4].plot(lap_telemetry_ver['Distance'], lap_telemetry_ver['DRS'], label='VER')
+ax[4].set(ylabel='DRS')
+
+# Hide x labels and tick labels for top plots and y ticks for right plots.
+for a in ax.flat:
+    a.label_outer()
+
+plt.show()
