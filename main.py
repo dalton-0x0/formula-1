2e7894828ed71a7a060e5123d4d2d82a9fde0548
Dalton 0x0 <dalton.0x0@gmail.com>
2022-12-30 18:42:59 -0700

    adds space format to function call. updates gitignore

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 from f1_2021_russia import generate_minisector_plot
 
 if __name__ == '__main__':
-   generate_minisector_plot(46, save=True, details=False)
+    generate_minisector_plot(46, save=True, details=False)
